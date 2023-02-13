5e2181006010ce8ba436ff8207fee5b05ba54bd3
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-02-13 20:41:20 +0300

    Initial Commit

---
+++ InfinityScrollList/InfinityScrollListApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinityScrollListApp.swift
+//  InfinityScrollList
+//
+//  Created by Abdullah Karaboğa on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct InfinityScrollListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
